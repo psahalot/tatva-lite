1a4a86f963c8828c60a31187f055729df8b53a58
Puneet Sahalot <puneetsahalot@gmail.com>
2014-04-16 00:51:43 +0530

    fixed incorrect file name

--- functions.php
+++ functions.php
@@ -412,7 +412,7 @@ function tatva_scripts_styles() {
 	}
 
 	// Include this script to envoke a button toggle for the main navigation menu on small screens
-	wp_enqueue_script( 'small-menu', trailingslashit( get_template_directory_uri() ) . 'assets/js/navigation.js', array( 'jquery' ), '20130130', true );
+	wp_enqueue_script( 'small-menu', trailingslashit( get_template_directory_uri() ) . 'assets/js/small-menu.js', array( 'jquery' ), '20130130', true );
 	
 
 }
